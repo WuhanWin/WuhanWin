62254ff7356d5d0d77cc7cdcaa015e6de19c5226
cleanbing <cleanbing@qq.com>
2020-01-30 14:52:11 +0800

    初始化版本，请查看 本地开发环境指南.docx 基于thinkcmf开发的第一个版本

---
+++ data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'ncov',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'root',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'cmf_',
+    //密钥
+    "AUTHCODE" => 'o5Jrgls9LWmxGIn7ZC',
+    //cookies
+    "COOKIE_PREFIX" => 'utwhlT_',
+);
